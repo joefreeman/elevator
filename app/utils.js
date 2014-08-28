a00804576139feb6e43e56c3114075a55c3f3f8a
Joe Freeman <joe.freeman@bitroot.com>
2014-08-28 17:53:37 +0200

    More random utils.

--- app/utils.js
+++ app/utils.js
@@ -6,6 +6,10 @@ App.Utils.randInt = function(start, end) {
   return Math.floor(App.Utils.randFloat(start, end));
 }
 
+App.Utils.randItem = function(list) {
+  return list[App.Utils.randInt(0, list.length)];
+}
+
 App.Utils.clamp = function(val, min, max) {
   return Math.max(min, Math.min(val, max));
 }
