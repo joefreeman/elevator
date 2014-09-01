bcc1519c83d92701f423bb9985a9d9834ec33fa0
Joe Freeman <joe.freeman@bitroot.com>
2014-09-01 21:05:49 +0200

    Refactor event loop into a function.

--- app/utils.js
+++ app/utils.js
@@ -13,3 +13,25 @@ App.Utils.randItem = function(list) {
 App.Utils.clamp = function(val, min, max) {
   return Math.max(min, Math.min(val, max));
 }
+
+App.Utils.runEventLoop = function(fn, interval) {
+  var running = true;
+  var lastUpdateTime = Date.now();
+  var control = {
+    stop: function() {
+      running = false;
+    },
+    start: function() {
+      lastUpdateTime = Date.now();
+      running = true;
+    }
+  };
+  setInterval(function() {
+    if (running) {
+      var startTime = Date.now();
+      fn(startTime - lastUpdateTime);
+      lastUpdateTime = startTime;
+    }
+  }, interval);
+  return control;
+}
